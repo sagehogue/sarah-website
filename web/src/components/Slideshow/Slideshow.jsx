8305884b7a3b9f039879556c825c927e0012338d
Sage Hogue <sagehogue@gmail.com>
2021-04-08 12:52:24 -0700

    fixed mobile view

--- web/src/components/Slideshow/Slideshow.jsx
+++ web/src/components/Slideshow/Slideshow.jsx
@@ -20,16 +20,41 @@ const SlideshowFrame = styled.div`
   // width: ${width}%;
   height: 650px;
   width: 650px;
-  background-color: whitesmoke;
+  // background-color: whitesmoke;
   z-index: ${props => (props.isActive ? "5" : "-1")};
   opacity: ${props => (props.isActive ? "1" : "0")};
   & .gatsby-image-wrapper {
     min-height: 650px;
-    margin: auto;
+    // margin: auto;
   }
   @media screen and (max-width: 700px) {
-    height: ${mobileHeight}vh;
+    top: 25vh;
+
     width: ${mobileWidth}%;
+    & .gatsby-image-wrapper {
+      min-height: ${mobileHeight}vw;
+      // min-height: 500px;
+      // margin: auto;
+    }
+  }
+  @media screen and (max-width: 600px) {
+    top: 15vh;
+  }
+  @media screen and (max-width: 550px) {
+    top: 20vh;
+  }
+  @media screen and (max-width: 500px) {
+    top: 25vh;
+  }
+  @media screen and (max-width: 450px) {
+    top: 30vh;
+
+    width: ${mobileWidth}%;
+    & .gatsby-image-wrapper {
+      min-height: ${mobileHeight}vw;
+      // min-height: 500px;
+      // margin: auto;
+    }
   }
 `
 
